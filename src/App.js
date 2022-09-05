b189e24ba62cc19fc3a61bdc23c18d818f6d9c75
Yevhen Malyi <malyje164@gmail.com>
2022-09-05 23:37:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import Form from './component/Form/Form'
 import './App.css';
 
 function App() {
+  const [todos,setTodos]=useState([])
+  const [allTodos,setAllTodos]=useState(0)
+  const [allCompleat,setAllCompleat]=useState(0)
+
+   useEffect(()=>{
+    setAllCompleat(todos.filter(todo=>todo.done ===true).length)
+  },[todos])
+ 
+  const putTodo=(value)=>{
+    if(value){
+      setTodos([...todos,{id:Date.now(),text:value,done:false}])
+      setAllTodos(allTodos+1)
+    }else{
+      alert('Please enter text in input')
+    }
+  }
+
+const toggleTodo =(id)=>{
+  setTodos(todos.map(todo=>{
+    if(todo.id !== id) return todo
+    return{
+      ...todo,
+      done:!todo.done
+    }
+  }))
+}
+const removeTodo=(id)=>{
+  setTodos(todos.filter(todo=>todo.id!== id))
+  setAllTodos(allTodos-1)
+}
+const clearAllTodos=()=>{
+  setTodos([]);
+  setAllTodos(0)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <h1 className="title">Todo list</h1>
+        <Form putTodo={putTodo}/>
+        <ul className="todos">
+        {
+           todos.map(todo=>{
+             return(
+               <li className={todo.done ?"todo done":"todo"} key={todo.id} onClick={e=>toggleTodo(todo.id)}>
+                 {todo.text}
+                 <button className="delet" onClick={e=>{
+                   e.stopPropagation();
+                   removeTodo(todo.id)
+                 }}>Delet</button>
+               </li>
+             )
+           })
+         }
+
+         <div className="info">
+           <span>All todos:{allTodos}</span>
+           <span>Compleat:{allCompleat}</span>
+         </div>
+         <button className="btn" onClick={clearAllTodos}>Delet All todo</button>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/component/Form/Form.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import "./Form.css";
+
+function Form(props) {
+    const [value, setValue]= useState('')
+  return (
+    <form className='form' onSubmit={e=>{
+        e.preventDefault();
+        props.putTodo(value);
+        setValue('');
+    }}>
+        <input type="text" placeholder='enter todo' className="input" value={value} 
+        onChange={e=>setValue(e.target.value)}/>
+    </form>
+  )
+}
+
+export default Form
